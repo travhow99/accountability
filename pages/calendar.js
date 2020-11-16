78cc2684f877296d5e8426a04323504ff0e3e4e1
travis <trav.horn@gmail.com>
2020-11-15 21:44:25 -0700

    Initial commit next.js app

---
+++ pages/calendar.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
+export default function Calendar() {
+    return (
+        <Layout>
+            <Head>
+                <title>Calendar</title>
+            </Head>
+            <h1>Calendar</h1>
+        </Layout>
+    );
+}
